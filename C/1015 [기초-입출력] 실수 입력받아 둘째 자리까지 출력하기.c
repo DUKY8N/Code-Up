2b7d7c3e79cac61e91fd9b992a2a3748c848f05a
Dukyoon <miso1061402ggm@gmail.com>
2019-09-22 02:29:43 +0900

    Correct: C 1015 ~ 1020

---
+++ C/1015 [기초-입출력] 실수 입력받아 둘째 자리까지 출력하기.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+	float num;
+	
+	scanf("%f", &num);
+	printf("%.2f", num);
+	
+	return 0;
+}
---
+++ C/1018 [기초-입출력] 시간 입력받아 그대로 출력하기.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+	int hour, minute;
+
+	scanf("%d:%d", &hour, &minute);
+	printf("%d:%d", hour, minute);
+
+	return 0;
+}
