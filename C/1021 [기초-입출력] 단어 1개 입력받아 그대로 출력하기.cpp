931b6ee4c29c348165980eb15f8fb3337fda8fdf
Dukyoon <miso1061402ggm@gmail.com>
2019-09-22 02:50:38 +0900

    Wrong: C 1021[2]

--- C/1021 [기초-입출력] 단어 1개 입력받아 그대로 출력하기.cpp
+++ C/1021 [기초-입출력] 단어 1개 입력받아 그대로 출력하기.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(void) {
-	char word[51]; //문자를 50개 저장하기 위해서는 char data[51] 로 선언하면 된다.
+	char word[51] = ""; //문자를 50개 저장하기 위해서는 char data[51] 로 선언하면 된다.
 	
 	scanf("%c", &word);
 	printf("%c", word);
