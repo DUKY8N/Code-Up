36ed4dc9b582bdd48fcda4e4c436c0356b7835b6
Dukyoon <miso1061402ggm@gmail.com>
2019-09-23 03:14:44 +0900

    Correct: C 1029 ~ 1030

--- C/1029 [기초-데이터형] 실수 1개 입력받아 그대로 출력하기2.c
+++ C/1029 [기초-데이터형] 실수 1개 입력받아 그대로 출력하기2.c
@@ -4,7 +4,9 @@ int main(void) {
 	double num;
 	
 	scanf("%lf", &num); // %d로 햇갈리지 말자! 
-	printf("%lf", num); 
+	printf("%.11lf", num); 
+	// "%d"는 소수점 7자리에서 반올림 되고,
+	// "%11d"는 소수점 12자리에서 반올림 된다. 
 	
 	return 0;
 }
